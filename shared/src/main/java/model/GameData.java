189c919e7283e9f09c2c0cfeb18ca9a04e0605db
porte <porterevans32@gmail.com>
2025-05-21 14:04:24 -0600

    Imp GameData

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,39 @@
 package model;
 
+import chess.ChessGame;
+
 public class GameData {
+    int gameID;
+    String whiteUsername;
+    String blackUsername;
+    String gameName;
+    ChessGame game;
+    public GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game){
+        this.gameID = gameID;
+        this.game = game;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
 }

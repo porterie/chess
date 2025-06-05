b42071dd4f8d7c985fd8f3d9b1602158ac74a05d
porte <porterevans32@gmail.com>
2025-06-05 15:00:12 -0600

    Prelogin Client done (?)

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -13,12 +13,11 @@ import java.util.Scanner;
 public class PreLoginClient {
     private final String serverUrl;
     private final ServerFacade server;
-    private LoginState state = LoginState.SIGNEDOUT;
     Gson gson = new Gson();
     public PreLoginClient(String serverUrl){
         this.serverUrl = serverUrl;
         server = new ServerFacade(serverUrl);
-        //TODO: add notification handler if neccessary
+
     }
 
     public String eval(String input) {
@@ -26,9 +25,9 @@ public class PreLoginClient {
             var tokens = input.toLowerCase().split(" ");
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            return switch (cmd) {//todo: correct methods for class
-                case "signin" -> register(params);
-                case "rescue" -> login(params);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
                 case "quit" -> "quit";
                 default -> help();
             };
@@ -41,22 +40,29 @@ public class PreLoginClient {
         //failed http will throw the response exception
         if(params.length==3){
             RegisterResult response = server.register(params[0], params[1], params[2]);
-            state=LoginState.SIGNEDIN;
             return String.format("Registered user %s", params[0]);
         }else{
             throw new ResponseException(400, "Expected: <username> <password> <email>");
         }
     }
 
-    public Object login(String... params) throws ResponseException {
+    public String login(String... params) throws ResponseException {
         if(params.length==2){
             LoginResult response = server.login(params[0], params[1]);
-            state=LoginState.SIGNEDIN;
             return String.format("Logged in user %s", params[0]);
         }else{
             throw new ResponseException(400, "Expected: <username> <password>");
         }
 
+    }
+
+    public String help() {
+        return """
+                - register <username> <password> <email>
+                - login <username> <password>
+                - quit
+                - help
+                """;
     }
 
 }
